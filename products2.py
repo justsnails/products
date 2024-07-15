4201f375cbb326838cb4a21c3f9083f6e2a31602
justsnails <loveserenasu@gmail.com>
2024-07-15 14:21:51 +0800

    add_new

---
+++ products2.py
@@ -0,0 +1,17 @@
+products = []
+print('歡迎來到好事多!')
+while True:
+    print('請問要購買什麼商品?')
+    buy = input('請輸入購買的商品 : ')
+    if buy == 'q':
+        print('歡迎再度光臨!')
+        break
+    price = input('請輸入商品價格 : ')
+    products.append([buy, price]) 
+for p in products:
+    print(p[0], '的價格為:', p[1])
+
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+    f.write('商品,價格\n')
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
